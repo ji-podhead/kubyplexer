01ded9e3c4be68c388dc1a101fe42c9e3721477c
J.I. /podhead <117015142+ji-soft@users.noreply.github.com>
2024-02-27 13:38:43 +0100

    rm some files

--- kubyplexer_js/kuby_py.py
+++
@@ -1,7 +0,0 @@
-# kuby_py.py
-import sys
-
-# Beispiel: Erhöhen Sie den Counter um   1
-new_counter_value =  1
-print("hello from python"+str(new_counter_value))
-sys.stdout.write(str(new_counter_value))
